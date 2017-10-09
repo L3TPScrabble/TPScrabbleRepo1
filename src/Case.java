7736e2af8562b57dbfc9e941447c94b61786d4ac
guillaume <guillaume.jouglet08@gmail.com>
2017-10-09 10:02:04 +0200

    scoreMot()

--- src/Case.java
+++ src/Case.java
@@ -4,12 +4,40 @@ public class Case {
 	private boolean estVide;
 	private Piece contenu;
 	
-	
+	public Case() {
+		this.id = 0;
+		this.estVide = true;
+		this.contenu = null;
+	}
 	public Case(int id) {
 		this.id = id;
 		this.estVide = true;
 		this.contenu = null;
 	}
+
+
+	public boolean estVide() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
+	public Piece getContenu()
+	{
+		return contenu;
+	}
 	
+	public void setContenu(Piece nouveauContenu)
+	{
+		contenu = nouveauContenu ;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int nouvelleId)
+	{
+		id = nouvelleId;
+	}
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,14 +1,31 @@
 
 public class Piece {
 	
-	private String lettre;
+	private char lettre;
 	private int points;
 	
 	
-	public Piece(String lettre, int points) {
+	public Piece(char lettre, int points) {
 		this.lettre = lettre;
 		this.points = points;
 	}
+	
+	public char getLettre(){
+		return lettre;
+	}
+	
+	public void setLettre(char nouvelleLettre){
+		lettre = nouvelleLettre;
+	}
+	
+	public int getPoints(){
+		return points;
+	}
+	
+	public void setPoints(int nouveauxPoints){
+		points= nouveauxPoints;
+	}
+
 	
 	
 }
