ed47e19464f548d980311a8800879ae1774badbf
YoxT <yoxtgame@live.fr>
2017-10-09 11:04:19 +0200

    a

--- src/interfaces/CaseInterface.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface CaseInterface {
-	
-	public boolean estVide();
-
-}
--- src/interfaces/SacInterface.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface SacInterface {
-	
-	public void retirerPieces(int nombre);
-
-}
