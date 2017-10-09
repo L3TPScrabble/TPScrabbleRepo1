e38903d1ca74e62e0217fdcb5f440995886f05e1
guillaume <guillaume.jouglet08@gmail.com>
2017-10-09 10:46:41 +0200

    addmoot()

--- src/Sac.java
+++ src/Sac.java
@@ -10,7 +10,39 @@ public class Sac {
 		this.pioche = pioche;	//définir la pioche de départ
 		this.piecesRestantes = 102;
 		this.sacVide = false;
+		pioche.getLettre()
 	}
 
+
+	public List<Piece> getPioche() {
+		return pioche;
+	}
+
+
+	public void setPioche(List<Piece> pioche) {
+		this.pioche = pioche;
+	}
+
+
+	public int getPiecesRestantes() {
+		return piecesRestantes;
+	}
+
+
+	public void setPiecesRestantes(int piecesRestantes) {
+		this.piecesRestantes = piecesRestantes;
+	}
+
+
+	public boolean isSacVide() {
+		return sacVide;
+	}
+
+
+	public void setSacVide(boolean sacVide) {
+		this.sacVide = sacVide;
+	}
+	
+	
 	
 }
