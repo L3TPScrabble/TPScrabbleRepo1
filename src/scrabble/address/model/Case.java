0c902878b2b899b75b8f43b9ee9224f5cd888979
Yoxt <32191843+Yoxt@users.noreply.github.com>
2017-11-27 14:37:23 +0100

    Allez les gars

---
+++ src/scrabble/address/model/Case.java
@@ -0,0 +1,60 @@
+package scrabble.address.model;
+
+import interfaces.CaseInterface;
+
+public class Case extends Plateau implements CaseInterface{
+	private int id;
+	private boolean estVide;
+	private Piece contenu;
+	private boolean jouable;
+	
+	public Case() {
+		this.id = 0;
+		this.estVide = true;
+		this.contenu = null;
+		
+	}
+	public Case(int id) {
+		this.id = id;
+		this.estVide = true;
+		this.contenu = null;
+	}
+
+
+	public boolean estVide() {
+		return this.estVide;
+	}
+	
+	public Piece getContenu()
+	{
+		return contenu;
+	}
+	
+	public void setContenu(Piece nouveauContenu)
+	{
+		contenu = nouveauContenu ;
+	}
+	
+	public boolean isJouable() {
+		return jouable;
+	}
+	public void setJouable(boolean b) {
+		this.jouable = true;
+	}
+
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int nouvelleId)
+	{
+		id = nouvelleId;
+	}
+	
+	public int bonusCase(CaseInterface c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
