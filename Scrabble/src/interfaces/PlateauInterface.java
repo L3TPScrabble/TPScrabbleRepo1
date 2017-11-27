65c68c80892e05641bb9ce53b7d10bbcddcd55f3
YoxT <yoxtgame@live.fr>
2017-11-27 14:51:15 +0100

    up2

--- Scrabble/src/interfaces/PlateauInterface.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface PlateauInterface {
-	
-	public int ScoreMot(String mot, int i, int j, String d);
-	public Boolean estPlein();
-	public void addMot(String mot, int i, int j, String d);
-	
-}
