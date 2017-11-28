f16b076c64cfab5ee96c375a7026d9d43ef508fe
Keras08 <32368994+Keras08@users.noreply.github.com>
2017-11-28 15:09:24 +0100

    Add files via upload
    Nouveau joueur

--- src/scrabble/address/view/ViewController.java
+++ src/scrabble/address/view/ViewController.java
@@ -1,9 +1,18 @@
 package scrabble.address.view;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.EventObject;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
@@ -14,9 +23,12 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
 import scrabble.address.mainapp.MainApp;
 import scrabble.address.model.Partie;
+
 
 public class ViewController {
 
@@ -39,6 +51,11 @@ public class ViewController {
 	
 	
 	@FXML
+	private Button btnNewPlayer;
+	
+	
+	
+	@FXML
 	private Label lettre4;
 	
 	@FXML
@@ -47,9 +64,15 @@ public class ViewController {
 	@FXML
 	private Label lettre6;
 	
+
+	
 	private MainApp mainApp;
 	
 	private Node node;
+	
+	private int compteur;
+	
+	
 	public ViewController(){
 		
 	}
@@ -64,13 +87,16 @@ public class ViewController {
 		
 		this.mainApp = mainApp;
 		try{
-		lettre0.setText(mainApp.getPartie().getParticipants().get(0).getMain().get(0).getLettre());
-		lettre1.setText(mainApp.getPartie().getParticipants().get(0).getMain().get(1).getLettre());
-		lettre2.setText(mainApp.getPartie().getParticipants().get(0).getMain().get(2).getLettre());
-		lettre3.setText(mainApp.getPartie().getParticipants().get(0).getMain().get(3).getLettre());
-		lettre4.setText(mainApp.getPartie().getParticipants().get(0).getMain().get(4).getLettre());
-		lettre5.setText(mainApp.getPartie().getParticipants().get(0).getMain().get(5).getLettre());
-		lettre6.setText(mainApp.getPartie().getParticipants().get(0).getMain().get(6).getLettre());
+		compteur = 0;
+		lettre0.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(0).getLettre());
+		lettre1.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(1).getLettre());
+		lettre2.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(2).getLettre());
+		lettre3.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(3).getLettre());
+		lettre4.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(4).getLettre());
+		lettre5.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(5).getLettre());
+		lettre6.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(6).getLettre());
+		
+		
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -103,6 +129,10 @@ public class ViewController {
 	                        //Get the textarea and place it into flowPane2 instead
 	                    	mainApp.getGP().add(this.node, l, k);
 	                    	success = true;
+	                    	System.out.println(event.getDragboard());
+	                    	System.out.println(this.node.toString());
+	                    	System.out.println(event.getSource().toString());
+	                    	System.out.println(event.getPickResult().getIntersectedNode().getId());
 	                    }
 	                    //Complete and consume the event.
 	                    event.setDropCompleted(success);
@@ -115,7 +145,8 @@ public class ViewController {
 	        }
 	
 	public void OnDragDetected(MouseEvent event){
-		   this.node = event.getPickResult().getIntersectedNode();;
+		try{
+		   this.node = (Text)event.getPickResult().getIntersectedNode();;
 		  Dragboard db = lettre0.startDragAndDrop(TransferMode.MOVE);
 
 		    // Put a string on a dragboard as an identifier
@@ -124,6 +155,10 @@ public class ViewController {
 		    db.setContent(content);
 		    //Consume the event
 		    event.consume();
+		}
+		catch(ClassCastException i){
+			i.printStackTrace();
+		}
 	}
 	public void DRAG_DROPPED(DragEvent event){
 		
@@ -147,16 +182,94 @@ public class ViewController {
             	int j = (int) event.getSceneY();
             	mainApp.getGP().add(this.node, i, j);
 		        success = true;
+		        	
 		    }
 		    //Complete and consume the event.
 		    event.setDropCompleted(success);
 		    event.consume();
 	}
-		public void OVER(DragEvent event){
+	
+	public void OVER(DragEvent event){
 	    if (event.getGestureSource() != mainApp.getGP()
 	            && event.getDragboard().hasString()) {
 	        event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
 	    }
 	    event.consume();
 	}
+	
+	public void ButtonClicked2(ActionEvent e) {
+		
+		if(e.getSource()==btnNewPlayer)
+		{
+			System.out.println(lettre0.getText());
+			
+			if(lettre0.getText().equals(" ")){
+				mainApp.getPartie().getParticipants().get(compteur).getMain().get(0).setLettre(mainApp.getPartie().getSac().piocher().getLettre());
+				lettre0.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(0).getLettre());
+			}
+			if(lettre1.getText().equals("")){
+				mainApp.getPartie().getParticipants().get(compteur).getMain().get(1).setLettre(mainApp.getPartie().getSac().piocher().getLettre());
+				lettre1.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(1).getLettre());
+			}
+			if(lettre2.getText().equals("")){
+				mainApp.getPartie().getParticipants().get(compteur).getMain().get(2).setLettre(mainApp.getPartie().getSac().piocher().getLettre());
+				lettre2.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(2).getLettre());
+			}
+			if(lettre3.getText().equals("")){
+				mainApp.getPartie().getParticipants().get(compteur).getMain().get(3).setLettre(mainApp.getPartie().getSac().piocher().getLettre());
+				lettre3.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(3).getLettre());
+			}
+			if(lettre4.getText().equals(" ")){
+				mainApp.getPartie().getParticipants().get(compteur).getMain().get(4).setLettre(mainApp.getPartie().getSac().piocher().getLettre());
+				lettre4.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(4).getLettre());
+			}
+			if(lettre5.getText().equals(" ")){
+				mainApp.getPartie().getParticipants().get(compteur).getMain().get(5).setLettre(mainApp.getPartie().getSac().piocher().getLettre());
+				lettre5.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(5).getLettre());
+			}
+			if(lettre6.getText().equals(" ")){
+				mainApp.getPartie().getParticipants().get(compteur).getMain().get(6).setLettre(mainApp.getPartie().getSac().piocher().getLettre());
+				lettre6.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(6).getLettre());
+			}
+			compteur++;
+			try{
+			lettre0.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(0).getLettre());
+			lettre1.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(1).getLettre());
+			lettre2.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(2).getLettre());
+			lettre3.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(3).getLettre());
+			lettre4.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(4).getLettre());
+			lettre5.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(5).getLettre());
+			lettre6.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(6).getLettre());
+			
+			}catch(Exception i){
+				compteur = 0;
+				lettre0.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(0).getLettre());
+				lettre1.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(1).getLettre());
+				lettre2.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(2).getLettre());
+				lettre3.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(3).getLettre());
+				lettre4.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(4).getLettre());
+				lettre5.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(5).getLettre());
+				lettre6.setText(mainApp.getPartie().getParticipants().get(compteur).getMain().get(6).getLettre());
+			}
+			
+		}
+	}
+	
+/*	private void bindToTime() {
+		  Timeline timeline = new Timeline(
+		    new KeyFrame(Duration.seconds(0),
+		      new EventHandler<ActionEvent>() {
+		        @Override public void handle(ActionEvent actionEvent) {
+		          Calendar time = Calendar.getInstance();
+		          SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+		          lettre1.setText("azra");
+		        }
+		      }
+		    ),
+		    new KeyFrame(Duration.seconds(1))
+		  );
+		  timeline.setCycleCount(Animation.INDEFINITE);
+		  timeline.play();
+	}*/
+
 }
